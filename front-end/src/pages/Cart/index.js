5569918e644bd3a8e31d0629762830bb10aa1510
Victor Dulap <victordulapit@gmail.com>
2021-11-03 18:47:55 +0200

    small fixes

--- front-end/src/pages/Cart/index.js
+++ front-end/src/pages/Cart/index.js
@@ -3,7 +3,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import Button from '../../components/Button';
 import './style.scss';
 
-import { FaTimes } from 'react-icons/fa';
 import { DELIEVERY_PRICE } from '../../utils/constants';
 import {
   decrementCount,
@@ -12,7 +11,6 @@ import {
   removeFromCart,
 } from '../../features/cartSlice';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
-import { Link } from 'react-router-dom';
 import CartItem from './components/CartItem';
 
 const Cart = () => {
@@ -47,6 +45,7 @@ const Cart = () => {
           {cart.length > 0 ? (
             cart.map((cartItem) => (
               <CartItem
+                key={cartItem.id}
                 cartItemId={cartItem.id}
                 itemId={cartItem.item._id}
                 imgUrl={cartItem.item.imgUrl}
@@ -82,45 +81,49 @@ const Cart = () => {
             </div>
           )}
         </section>
-        <section className="total">
-          <p>
-            <strong>
-              <span className="letter-spacing">sub-total:</span> &#36;
-              {cartTotal.toFixed(2)}
-            </strong>
-          </p>
-        </section>
-        <section className="checkout">
-          <h2 className="letter-spacing">
-            <strong>
-              total: &#36;{(cartTotal + DELIEVERY_PRICE).toFixed(2)}
-            </strong>
-          </h2>
-          <div className="checkout-price">
-            <p className="letter-spacing">
-              <strong>sub-total</strong>
-            </p>
-            <p className="price">&#36;{cartTotal.toFixed(2)}</p>
-          </div>
-          <div className="checkout-price">
-            <p className="letter-spacing">
-              <strong>delievery</strong>
-            </p>
-            <p className="price">&#36;{DELIEVERY_PRICE.toFixed(2)}</p>
-          </div>
-          <div
-            className="checkout-btn
+        {cart.length > 0 && (
+          <>
+            <section className="total">
+              <p>
+                <strong>
+                  <span className="letter-spacing">sub-total:</span> &#36;
+                  {cartTotal.toFixed(2)}
+                </strong>
+              </p>
+            </section>
+            <section className="checkout">
+              <h2 className="letter-spacing">
+                <strong>
+                  total: &#36;{(cartTotal + DELIEVERY_PRICE).toFixed(2)}
+                </strong>
+              </h2>
+              <div className="checkout-price">
+                <p className="letter-spacing">
+                  <strong>sub-total</strong>
+                </p>
+                <p className="price">&#36;{cartTotal.toFixed(2)}</p>
+              </div>
+              <div className="checkout-price">
+                <p className="letter-spacing">
+                  <strong>delievery</strong>
+                </p>
+                <p className="price">&#36;{DELIEVERY_PRICE.toFixed(2)}</p>
+              </div>
+              <div
+                className="checkout-btn
           "
-          >
-            <Button
-              dark
-              size="l"
-              block
-              text="CHECKOUT"
-              disabled={cartTotal <= 0}
-            />
-          </div>
-        </section>
+              >
+                <Button
+                  dark
+                  size="l"
+                  block
+                  text="CHECKOUT"
+                  disabled={cartTotal <= 0}
+                />
+              </div>
+            </section>
+          </>
+        )}
       </div>
     </main>
   );
