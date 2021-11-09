dece776f1752bb7567efd691501a7fd1babac521
Victor Dulap <victordulapit@gmail.com>
2021-11-09 19:08:40 +0200

    updated the data we send to client

--- back-end/controllers/order.js
+++ back-end/controllers/order.js
@@ -11,12 +11,23 @@ const getAllOrders = async (req, res) => {
 const createOrder = async (req, res) => {
   const order = await Order.create(req.body);
 
+  const { shippingDetails, total, cart } = req.body;
+
+  const cartLength = cart.reduce((prev, curr) => prev + curr.count, 0);
+
   const mailData = {
     from: 'vdclothes.step@gmail.com', // sender address
-    to: req.body.shippingDetails.email, // list of receivers
+    to: shippingDetails.email, // list of receivers
     subject: 'Your order at VD clothes',
     text: 'Your order at VD clothes',
-    html: '<b>Your order at VD clothes </b><br> This is our first message sent with Nodemailer<br/>',
+    html: `
+      <h1>Thanks ${shippingDetails.firstName} ${shippingDetails.lastName} for ordering at VD clothes</h1>
+      <p>Your total for the order is: ${total}$ for ${cartLength} items!</p>
+      <br/>
+      <p>Order will be delieverd to ${shippingDetails.country}, ${shippingDetails.city}, ${shippingDetails.postcode} by 30 days.</p>
+      <br/>
+      <p>For more information, contact vdclothes.step@gmail.com</p>
+    `,
   };
 
   transporter.sendMail(mailData, (err, info) => {
