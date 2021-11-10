e9671f142a3d0469b0ca22426689d87f8565f0ef
Victor Dulap <victordulapit@gmail.com>
2021-11-10 16:06:58 +0200

    changed toggle function on filter criterias

--- front-end/src/pages/Shop/components/FilterModal.js
+++ front-end/src/pages/Shop/components/FilterModal.js
@@ -3,19 +3,29 @@ import { VscClose } from 'react-icons/vsc';
 import './FilterModal.scss';
 import { v4 as uuid } from 'uuid';
 
-const FilterModal = ({ closeModal }) => {
-  const filterOptions = [
-    {
-      title: 'Categories',
-      options: ['shoes', 't-shirts'],
-    },
-    {
-      title: 'Brands',
-      options: ['nike', 'adidas'],
-    },
-  ];
+const initialFilterOptions = [
+  {
+    title: 'Categories',
+    options: ['shoes', 't-shirts'],
+    show: false,
+  },
+  {
+    title: 'Brands',
+    options: ['nike', 'adidas'],
+    show: false,
+  },
+];
 
-  const [toggledDropdowns, setToggledDropdowns] = useState({});
+const FilterModal = ({ closeModal }) => {
+  const [filterOptions, setFilterOptions] = useState(initialFilterOptions);
+
+  const toggleShowFilterOptions = (index) => {
+    const itemByIndex = filterOptions[index];
+    const updatedItem = { ...itemByIndex, show: !itemByIndex.show };
+    const newState = [...filterOptions];
+    newState[index] = updatedItem;
+    setFilterOptions(newState);
+  };
 
   return (
     <div className="filter-modal">
@@ -27,22 +37,17 @@ const FilterModal = ({ closeModal }) => {
           <h2 className="letter-spacing">FILTER</h2>
         </header>
         <section className="filter-options-container">
-          {filterOptions.map((opt) => (
+          {filterOptions.map((opt, index) => (
             <div key={uuid()} className="filter-option-container">
               <p
-                onClick={() =>
-                  setToggledDropdowns((old) => ({
-                    ...old,
-                    [opt.title]: !old[opt.title],
-                  }))
-                }
+                onClick={() => toggleShowFilterOptions(index)}
                 className="filter-option-title"
               >
                 {opt.title}
               </p>
               <div
                 className="filter-options"
-                style={toggledDropdowns[opt.title] ? {} : { display: 'none' }}
+                style={opt.show ? {} : { display: 'none' }}
               >
                 {opt.options.map((o) => (
                   <p key={uuid()} className="filter-option">
