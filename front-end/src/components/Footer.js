712f2cb3be4055991f56d7d2ae0e0b84264cf65f
Victor Dulap <victordulapit@gmail.com>
2021-11-15 10:49:22 +0200

    added footer, fixed some css

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Footer.scss';
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="container">footer</div>
+      <div className="container">
+        <p>&copy; {new Date().getFullYear()} VD Clothes</p>
+        <Link to="/">Home</Link>
+      </div>
     </footer>
   );
 };
