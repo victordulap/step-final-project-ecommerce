f8bb36bc5079df550450b1146eea5a1737ecb1da
Victor Dulap <victordulapit@gmail.com>
2021-11-29 19:57:47 +0200

    add remove category in cms

--- backoffice/src/pages/Categories/Category.js
+++ backoffice/src/pages/Categories/Category.js
@@ -1,19 +1,36 @@
 import React, { useEffect } from 'react';
-import { Input, Typography, Form } from 'antd';
+import { Input, Typography, Form, Button, message, Divider } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
-import { getCategory } from '../../features/Categories/CategoriesActions';
+import { getCategory, removeCategory } from '../../features/Categories/CategoriesActions';
 import FormWrap from '../../components/FormWrap';
-import { useParams } from 'react-router';
+import { useNavigate, useParams } from 'react-router';
 import WrappedSpinner from '../../components/WrappedSpinner';
+import { STATE_STATUSES } from '../../util/constants';
+import { categoryActions } from '../../features/Categories/CategoriesSlice';
 
 const Category = () => {
-  const { category, isLoading } = useSelector(({ categories }) => categories);
+  const { category, isLoading, status } = useSelector(({ categories }) => categories);
   const dispatch = useDispatch();
   const { categoryId } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     dispatch(getCategory(categoryId));
   }, [dispatch, categoryId]);
+
+  useEffect(() => {
+    if (status === STATE_STATUSES.SUCCESS) {
+      message.success('category removed!');
+      dispatch(categoryActions.resetStatus());
+      navigate('/categories');
+    } else if (status === STATE_STATUSES.ERROR) message.error('category not removed!');
+  }, [dispatch, navigate, status]);
+
+  const deleteCategory = () => {
+    if (category) {
+      dispatch(removeCategory(category._id));
+    }
+  };
 
   const formFields = category
     ? [
@@ -53,6 +70,11 @@ const Category = () => {
             </Form.Item>
           ))}
         </Form>
+
+        <Divider />
+        <Button onClick={deleteCategory} danger type="primary" loading={isLoading}>
+          Delete brand
+        </Button>
       </FormWrap>
     </>
   );
