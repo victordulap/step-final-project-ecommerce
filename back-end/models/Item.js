6ade70854e9e9fde1e83c2fc4048ff847dfd6e43
Victor Dulap <victordulapit@gmail.com>
2021-12-01 19:37:53 +0200

    add timestamp to item model

--- back-end/models/Item.js
+++ back-end/models/Item.js
@@ -1,45 +1,48 @@
 const mongoose = require('mongoose');
 
-const CategorySchema = mongoose.Schema({
-  title: {
-    type: String,
-    trim: true,
-    required: [true, 'Please provide item title'],
+const CategorySchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: [true, 'Please provide item title'],
+    },
+    brandId: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Brand',
+      required: true,
+    },
+    categoryIds: {
+      type: [mongoose.Schema.ObjectId],
+      ref: 'Category',
+      required: true,
+    },
+    sizes: {
+      type: [String],
+      required: true,
+    },
+    color: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    imgUrl: {
+      type: String,
+      required: true,
+    },
+    available: {
+      type: Boolean,
+      default: true,
+    },
   },
-  brandId: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'Brand',
-    required: true,
-  },
-  categoryIds: {
-    type: [mongoose.Schema.ObjectId],
-    ref: 'Category',
-    required: true,
-  },
-  sizes: {
-    type: [String],
-    required: true,
-  },
-  color: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  imgUrl: {
-    type: String,
-    required: true,
-  },
-  available: {
-    type: Boolean,
-    default: true,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Item', CategorySchema);
