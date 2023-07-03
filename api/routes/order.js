f58316e645a6e2dafdb01591edaf6524e0658b6c
Victor Dulap <victordulapit@gmail.com>
2023-07-03 19:50:56 +0300

    add create order

--- api/routes/order.js
+++ api/routes/order.js
@@ -4,7 +4,7 @@ const authentication = require('../middleware/authentication');
 
 const { getAllOrders, createOrder, getOrder, updateOrder, removeOrder } = require('../controllers/order');
 
-// router.route('/').post(createOrder);
+router.route('/').post(createOrder);
 // router.route('/').get(getAllOrders).post(createOrder);
 // router.route('/:id').get(getOrder).patch(authentication, updateOrder).delete(authentication, removeOrder);
 
