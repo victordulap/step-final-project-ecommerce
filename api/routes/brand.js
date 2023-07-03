17dc7a2463e5ebedaf433899746ca27ef3926dfe
Victor Dulap <victordulapit@gmail.com>
2023-07-03 20:00:59 +0300

    uncomment more api functions

--- api/routes/brand.js
+++ api/routes/brand.js
@@ -4,7 +4,7 @@ const authentication = require('../middleware/authentication');
 
 const { getAllBrands, getBrand, addBrand, removeBrand, updateBrand } = require('../controllers/brand');
 
-router.route('/').get(getAllBrands);//.post(authentication, addBrand);
-router.route('/:id').get(getBrand);//.delete(authentication, removeBrand).put(authentication, updateBrand);
+router.route('/').get(getAllBrands).post(authentication, addBrand);
+router.route('/:id').get(getBrand).delete(authentication, removeBrand).put(authentication, updateBrand);
 
 module.exports = router;
--- api/routes/order.js
+++ api/routes/order.js
@@ -4,8 +4,8 @@ const authentication = require('../middleware/authentication');
 
 const { getAllOrders, createOrder, getOrder, updateOrder, removeOrder } = require('../controllers/order');
 
-router.route('/').post(createOrder);
-// router.route('/').get(getAllOrders).post(createOrder);
-// router.route('/:id').get(getOrder).patch(authentication, updateOrder).delete(authentication, removeOrder);
+// router.route('/').post(createOrder);
+router.route('/').get(getAllOrders).post(createOrder);
+router.route('/:id').get(getOrder).patch(authentication, updateOrder).delete(authentication, removeOrder);
 
 module.exports = router;
